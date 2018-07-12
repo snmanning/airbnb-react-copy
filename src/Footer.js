6b32a4b45a279d34cf9edf7f5ca26e6d9e319b69
Sarah Manning <sarahnmanning@gmail.com>
2018-07-12 19:38:33 -0400

    created footer component and styled it

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Footer.css';
+
+const Footer = (props) => 
+    <footer className='Footer-container'>
+        No smelly feet down here in the footer
+    </footer>
+
+
+Footer.propTypes = {}
+
+export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 import './Header.css';
 import PropTypes from 'prop-types';
 
-const Header = (props) => (
+const Header = (props) => 
     <header className='Header-container' >
         <h1> {props.title} </h1>
     </header>
-)
+
 
 Header.propTypes = {
     title: PropTypes.string.isRequired
